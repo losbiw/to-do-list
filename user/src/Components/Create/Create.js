22602e8af57bbbfdc0af955e1d19f5f02ca53234
losbiw <skorinov41@gmail.com>
2020-12-03 01:09:31 +0200

    Add create buttons events

--- user/src/Components/Create/Create.js
+++ user/src/Components/Create/Create.js
@@ -1,31 +1,79 @@
-import React from 'react'
+import React, { Component } from 'react'
 import './Create.css'
 
-export default function Create(props){
-    const { handleCreation, tasks, current } = props;
-    
-    const handleNewTaskCreation = () => {
-        tasks[current].list.push('New task')
+export default class Create extends Component{
+    constructor(props){
+        super(props);
+
+        this.state = {
+            areOptionsActive: false
+        }
+    }
+
+    componentDidMount(){
+        document.addEventListener('click', this.handleClickOutside);
+    }
+
+    componentWillUnmount(){
+        document.removeEventListener('click', this.handleClickOutside);
+    }
+
+    handleClickOutside = e => {
+        const { id } = e.target;
+
+        if(id !== 'add' && id !== 'task' && id !== 'category'){
+            this.setState({
+                areOptionsActive: false
+            })
+        }
+    }
+
+    handleNewTaskCreation = () => {
+        const { handleCreation, tasks, current } = this.props;
+
+        tasks[current].list.push({
+            value: 'New Task',
+            key: Math.random()
+        })
         handleCreation({ tasks })
     }
 
-    const handleNewCategoryCreation = () => {
+    handleNewCategoryCreation = () => {
+        const { handleCreation, tasks } = this.props;
+
         tasks.push({
-            category: 'new',
+            category: 'category',
             list: []
         });
 
         handleCreation({ tasks });
     }
 
-    return(
-        <div id="create">
-            <button className="add">Cock</button>
+    handleAddClick = () => {
+        this.setState({
+            areOptionsActive: true
+        })
+    }
 
-            <div className="options">
-                <button onClick={ handleNewTaskCreation }>Task</button>
-                <button onClick={ handleNewCategoryCreation }>Category</button>
+    render(){
+        const { handleNewTaskCreation, handleNewCategoryCreation, handleAddClick } = this;
+        
+        return(
+            <div id="create">
+                { this.state.areOptionsActive 
+                  ? <div id="options">
+                        <button id='task' onClick={ handleNewTaskCreation }>
+                            Task
+                        </button>
+                        <button id='category' onClick={ handleNewCategoryCreation }>
+                            Category
+                        </button>
+                    </div>
+                  : <button id="add" onClick={ handleAddClick }>
+                      Cock
+                    </button>
+                }
             </div>
-        </div>
-    )
+        )
+    }
 }
