2c5ad88e858f89e5ae3d5be74688dabbe3670b98
losbiw <skorinov41@gmail.com>
2020-12-24 15:07:29 +0200

    Update default user schema

--- server/models/User.js
+++ server/models/User.js
@@ -2,7 +2,12 @@ const mongoose = require('mongoose');
 
 const User = new mongoose.Schema({
     googleID: String,
-    tasks: { type: 'Array', default: [] }
+    tasks: { type: 'Array', default: [{
+        category: 'General',
+        list: [
+            { value: 'Your first task!' },
+        ]
+    }] }
 })
 
 module.exports = mongoose.model('Users', User);
