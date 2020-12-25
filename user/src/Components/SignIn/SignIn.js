5b0eed8cfbbd95e54cf8fa44eb9d37e66ce545c3
losbiw <skorinov41@gmail.com>
2020-12-25 02:10:23 +0200

    Add signin page

---
+++ user/src/Components/SignIn/SignIn.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import { capitalizeFirstLetter } from 'modules/convert'
+import './SignIn.css'
+
+export default function SignIn(_props){
+    const options = {
+        google: {
+            img: 'https://img-authors.flaticon.com/google.jpg',
+            url: 'http://localhost:5000/auth/google'
+        }
+    }
+    
+    return(
+        <div id="sign-in">
+            <h1>Sign in</h1>
+
+            <ul>
+                {
+                    Object.keys(options).map((strategy) => {
+                        const { img, url } = options[strategy];
+
+                        return(
+                            <li key={ strategy }>
+                                <a href={ url }>
+                                    <img src={ img } />
+                                    <p>{ `Sign in with ${ capitalizeFirstLetter(strategy) }` }</p>
+                                </a>
+                            </li>
+                        )
+                    })
+                }
+            </ul>
+        </div>
+    )
+}
