fd928d3d2cbee6a29eb8dd983975ee949465b003
losbiw <skorinov41@gmail.com>
2020-12-25 23:57:40 +0200

    Update user schema type

--- server/models/User.js
+++ server/models/User.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const User = new mongoose.Schema({
     googleID: String,
-    tasks: { type: 'Array', default: [{
+    tasks: { type: mongoose.Schema.Types.Mixed, default: [{
         category: 'General',
         list: [
             { value: 'Your first task!' },
