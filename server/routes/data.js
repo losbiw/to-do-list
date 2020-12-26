1b9d20c48f321f6aaa319c42a837c4126f58d21d
losbiw <skorinov41@gmail.com>
2020-12-26 02:07:05 +0200

    Add user data update logic

--- server/routes/data.js
+++ server/routes/data.js
@@ -1,7 +1,20 @@
 const router = require('express').Router();
+const User = require('../models/User');
 
 router.get('/', (req, res) => {
     res.send(req.user);
+});
+
+router.post('/update', async(req, res) => {
+    const { user, body } = req;
+    const { googleID } = user;
+
+    const updated = await User.findOneAndUpdate(
+        { googleID }, 
+        { tasks: body }
+    );
+
+    res.send(updated);
 });
 
 router.get('/logout', (req, res) => {
--- server/server.js
+++ server/server.js
@@ -14,7 +14,7 @@ require('./routes/passportSetup');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect(process.env.MONGO_DB_CONNECT, () => {
+mongoose.connect(process.env.MONGO_DB_CONNECT, { useFindAndModify: false }, () => {
     console.log('MongoDB is connected');
 });
 
