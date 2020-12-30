2c702e90152be88d4fa6226655594ff477d743d5
losbiw <skorinov41@gmail.com>
2020-12-30 10:40:22 +0200

    Replace plain webmanifest with webpack module

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
+const WebpackPwaManifest = require('webpack-pwa-manifest')
 
 module.exports = {
     entry: './user/src/index.js',
@@ -35,5 +36,23 @@ module.exports = {
                 to: 'favicons'
             }]
         }),
+        new WebpackPwaManifest({
+            name: 'Todo List',
+            short_name: '',
+            fingerprints: false,
+            publicPath: '/',
+            includeDirectory: true,
+            description: 'Yet another todo app no one asked for :pogChamp:',
+            icons: [
+              {
+                src: path.resolve('user/src/assets/favicons/android-chrome-192x192.png'),
+                sizes: [192]
+              },
+              {
+                src: path.resolve('user/src/assets/favicons/android-chrome-512x512.png'),
+                size: [512]
+              }
+            ]
+        })
     ]
 }
